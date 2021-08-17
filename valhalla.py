84d824074b65d32d5015165e02b6f1d07f3b5a33
Bastien <bastien.bournas@outlook.com>
2021-08-17 10:33:46 +0100

    Add getBadChars and stub exploit function

--- valhalla.py
+++ valhalla.py
@@ -5,6 +5,18 @@ import sys,socket
 import string
 import time
 
+#############################################################################################
+#                                                                                           #
+#   exploit                                                                                 #
+#   Cookie-cutter function to exploit the application                                       #
+#                                                                                           #
+#   before : if set, will be prepended to the exploit                                       #
+#   ip : ip address where application is hosted                                             #
+#   port : port where application is listening                                              #
+#                                                                                           #
+#############################################################################################
+def exploit(before = None, ip = None, port = None):
+    print('[+] Exploit mode', file=sys.stderr)
 
 #############################################################################################
 #                                                                                           #
@@ -19,12 +31,14 @@ import time
 #   character (='A'): the character to fill strings with                                    #
 #   randomCharacter (=False): if set to True, a random character is chosen each time        #
 #   before : if set, will be prepended to each buffer buffer entry                          #
-#   ip : ip address where program is hosted                                                 #
-#   port : port where program is listening                                                  #
+#   ip : ip address where application is hosted                                             #
+#   port : port where application is listening                                              #
 #                                                                                           #
 #############################################################################################
 def fuzz(bufferEntriesNumber, growingFactor = 100, character = "A", randomCharacter = False, before = None, 
         ip = None, port = None):
+    print('[+] Fuzz mode', file=sys.stderr)
+
     buffer = []
     counter = growingFactor
 
@@ -51,8 +65,8 @@ def fuzz(bufferEntriesNumber, growingFactor = 100, character = "A", randomCharac
 #   If ip and port are given, send it via socket, otherwise on stdout                       #
 #                                                                                           #
 #   buffer : buffer to send as output                                                       #
-#   ip : ip address where program is hosted                                                 #
-#   port : port where program is listening                                                  #
+#   ip : ip address where application is hosted                                             #
+#   port : port where application is listening                                              #
 #                                                                                           #
 #############################################################################################
 def send(buffer, ip = None, port = None):
@@ -88,8 +102,8 @@ def stdoutSend(buffer):
 #   Send the given buffer entry by entry, with possibility to break each time               #
 #                                                                                           #
 #   buffer : buffer to send on the socket (ip, port)                                        #
-#   ip : ip address where program is hosted                                                 #
-#   port : port where program is listening                                                  #
+#   ip : ip address where application is hosted                                             #
+#   port : port where application is listening                                              #
 #                                                                                           #
 #############################################################################################
 def socketSend(buffer, ip, port):
@@ -118,11 +132,32 @@ def socketSend(buffer, ip, port):
         finally:
             s.close()
 
+#############################################################################################
+#                                                                                           #
+#   getBadChars                                                                             #
+#   Randomly displays a beautiful viking banner                                             #
+#                                                                                           #
+#############################################################################################
+def getBadChars():
+    # Generated with :
+    # for x in range(1, 256):
+    #     print("\\x" + "{:02x}".format(x), end='')
+    return "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\
+    \x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\
+    \x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\
+    \x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\
+    \x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\
+    \x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\
+    \x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\
+    \xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\
+    \xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\
+    \xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\
+    \xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
 
 #############################################################################################
 #                                                                                           #
 #   banner                                                                                  #
-#   Randomly displays a beautiful viking banner                                             #
+#   Randomly displays a beautifull viking banner                                            #
 #                                                                                           #
 #############################################################################################
 def banner():
@@ -205,19 +240,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=text)
 
     # General options
-    parser.add_argument("-V", "--version", help="show program version", action="store_true")
-    parser.add_argument("-s", "--silent", help="hide beautiful viking banner", action="store_true")
+    parser.add_argument("-V", "--version", help="show valhalla version", action="store_true")
+    parser.add_argument("-s", "--silent", help="hide beautiful viking banner (SHAME, you'll go to helheim !)", action="store_true")
+    parser.add_argument("-b", "--before", help="When sending data (fuzz or exploit), this string is prepend first before each entry of the buffer")
 
     # Socket communication options
-    parser.add_argument("-H", "--host", help="set the ip address where program is hosted")
-    parser.add_argument("-p", "--port", help="set the port where program is listening")
+    parser.add_argument("-H", "--host", help="set the ip address where application is hosted")
+    parser.add_argument("-p", "--port", help="set the port where application is listening")
 
     # Fuzz options
     parser.add_argument("-f", "--fuzz", help="fuzz the output")
     parser.add_argument("-g", "--grow", help="When fuzzing, the lenght is increased by this number each time (100 by default)")
     parser.add_argument("-c", "--char", help="When fuzzing, the buffer is filled with this character ('A' by default)")
     parser.add_argument("-r", "--random", help="When fuzzing, the buffer is filled random character instead", action="store_true")
-    parser.add_argument("-b", "--before", help="When fuzzing, this string is send first before the buffer")
+
+    # Exploit options
+    parser.add_argument("-e", "--exploit", help="exploit the application", action="store_true")
     
     args = parser.parse_args()
 
@@ -225,6 +263,8 @@ if __name__ == "__main__":
         banner()
     if (args.version):
         print ("Valhalla version 0.1")
+    elif (args.exploit):
+        exploit(ip=args.host, port=args.port)
     elif (args.fuzz):
         grow = args.grow
         if not (grow):
@@ -234,3 +274,4 @@ if __name__ == "__main__":
             char = "A"
         fuzz(bufferEntriesNumber=args.fuzz, growingFactor=int(grow), character=char, randomCharacter=args.random, before=args.before,
             ip=args.host, port=args.port)
+
